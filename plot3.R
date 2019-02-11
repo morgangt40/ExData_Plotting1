c015441b6455f128be813236432bcf9ccffa2fd2
morgangt40 <47106804+morgangt40@users.noreply.github.com>
2019-02-11 15:27:41 +0000

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,20 @@
+##read and format data
+data<-read.delim("household_power_consumption.txt",sep = ";")
+data$DT<-paste(data$Date,data$Time)
+data$DT<-strptime(data$DT,format = "%d/%m/%Y %H:%M:%S")
+data$Global_active_power<-as.numeric(as.character(data$Global_active_power))
+data$Global_reactive_power<-as.numeric(as.character(data$Global_reactive_power))
+data$Voltage<-as.numeric(as.character(data$Voltage))
+data$Sub_metering_1<-as.numeric(as.character(data$Sub_metering_1))
+data$Sub_metering_2<-as.numeric(as.character(data$Sub_metering_2))
+data$Sub_metering_3<-as.numeric(as.character(data$Sub_metering_3))
+data<-data[data$DT>="2007-02-01 00:00:0"& data$DT<="2007-02-02 23:59:00",]
+data<-na.omit(data)
+
+##creating plot and png file
+png(filename = "plot2.png")
+plot(data$DT,data$Sub_metering_1,type="l",ylab="Energy sub metering",xlab = "")
+lines(data$DT,data$Sub_metering_2,col="red")
+lines(data$DT,data$Sub_metering_3,col="blue")
+legend("topright",lty=c(1,1,1),c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"))
+dev.off()
